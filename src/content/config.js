ba7c3efcad25ead8d3b9845cd051ad91c078eb67
GaiheiluKamei <cc@gaiheilukamei.cc>
2024-02-20 16:36:09 +0800

    blog v1.0

---
+++ src/content/config.js
@@ -0,0 +1,12 @@
+import {defineCollection, z} from 'astro:content'
+
+const posts = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        // z.coerce is a method used to transform the data type of a field, z.coerce.date() is used to transform a string into a Date object
+        publishedAt: z.coerce.date().optional(),
+        updatedAt: z.coerce.date().optional(),
+    })
+})
+
+export const collections = {posts}
